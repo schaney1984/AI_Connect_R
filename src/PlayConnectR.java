f385eec8d319d5460afb76b69fb4bb4f9b9eb669
Steven Chaney <steven_chaney@hotmail.com>
2015-10-12 00:50:28 -0400

    test commit

--- src/PlayConnectR.java
+++ src/PlayConnectR.java
@@ -1,5 +1,6 @@
 public class PlayConnectR {
     public static void main (String[] args) {
         System.out.println("Time to write some code...");
+        //making some changes to see if git is working now
     }
 }
